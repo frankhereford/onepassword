69e04f31ca817b24960129ebaf018a4422ba0897
Frank Hereford <frankhereford@users.noreply.github.com>
2023-04-03 22:47:17 -0500

    Python 1PW demo

--- compute_answer.py
+++ compute_answer.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python3
-# import the library needed to do a shasum of a string
-import hashlib
 import os
-from onepasswordconnectsdk.client import Client, new_client
+import hashlib
 
-#import library to set environment from .env file
 from dotenv import load_dotenv
-
-#load environment variables from .env
+from onepasswordconnectsdk.client import Client, new_client
+import onepasswordconnectsdk
 
 load_dotenv()
 
+# ⚠️⚠️⚠️
+# ! THIS VALUE IS A SECRET. It is the most important secret.
+# ! This is the only secret that is not stored in 1Password,
+# ! but it's also fine to store in 1PW as well.
+# * It's like having access to the vault as if you were in the 1PW UI.
+# * They can be configured to have a shelf-life and expire after a given time.
+ONEPASSWORD_CONNECT_TOKEN = os.getenv("OP_API_TOKEN")
+# ⚠️⚠️⚠️
 
-# ! Important Note: This string is not a secret
-VAULT_ID = "quvhrzaatbj2wotsjrumx3f62a" # Discovery day
+# if the ONEPASSWORD_CONNECT_TOKEN variable is empty, prompt the user for it
+if not ONEPASSWORD_CONNECT_TOKEN:
+    ONEPASSWORD_CONNECT_TOKEN = input("Enter your OnePassword Connect API token: ")
+
+# * None of the following values are secrets
+ONEPASSWORD_CONNECT_HOST = 'https://nothingbut.flowers'
 ITEM_NAME = "Secret for SHA"
+VAULT_ID = "quvhrzaatbj2wotsjrumx3f62a" # Discovery day
 
+# * This is how you define the secrets you want to pull out of 1Password
+REQUIRED_SECRETS = {
+    "sha_input_secret": {
+        "opitem": ITEM_NAME,
+        "opfield": ".password",
+        "opvault": VAULT_ID,
+    },
+}
 
-TOKEN = os.getenv("OP_API_TOKEN")
-# if the TOKEN variable is empty, prompt the user for it
-if not TOKEN:
-    TOKEN = input("Enter your API token: ")
+# * Open up a client instance to the 1Password Connect API, which is /our/ service.
+# * In this case, it's an ECS cluster.
+client: Client = new_client(ONEPASSWORD_CONNECT_HOST, ONEPASSWORD_CONNECT_TOKEN)
+SECRETS = onepasswordconnectsdk.load_dict(client, REQUIRED_SECRETS)
+print(SECRETS)
+
 
 # write a function named main which requests the user to enter a string with the prompt "Enter the input string"
 def main():
+    print("We're going to take a string you input and concatenate it with a secret from 1Password and run the result through SHA1.")
     user_supplied_string = input("Enter the input string: ")
-    
-    # creates a client by supplying hostname and 1Password Connect API token
-    client: Client = new_client("https://nothingbut.flowers", TOKEN)
-
-
-
-
-    secret = client.get_item_by_title(f"{ITEM_NAME}", f"{VAULT_ID}")
-
-    #print(secret)
-    print(type(secret.fields[0]))
-
-    value = [item for item in secret.fields if item.get('id') == "PASSWORD"]
-    print(value)
-
-    print(f"Our secret for '{secret.fields[2].label}' is '{secret.fields[2].value}', 🎉")
-
-    print(shasum(user_supplied_string, secret.fields[2].value))
+    print("Resulting SHA1 Hash: ", shasum(user_supplied_string, SECRETS["sha_input_secret"]))
 
 # write a function that takes two strings and concatenates them with a hyphen in the middle and computes the shasum
 def shasum(s1, s2):
